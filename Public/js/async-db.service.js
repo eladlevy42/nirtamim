39965cee5ec76bca97c34b8d4e6742da42d97b8f
NirForster <nirforster@gmail.com>
2024-06-05 13:57:06 +0300

    added async functions

--- Public/js/async-db.service.js
+++ Public/js/async-db.service.js
@@ -1,47 +1,16 @@
-async function postStore(storeData) {
-    try {
-        const response = await storeService.postStore(storeData);
-        return response;
-    } catch (error) {
-        console.error('Error posting store:', error);
-        throw error;
-    }
-}
-async function updateStore(storeId, updateData) {
-    try {
-        const response = await storeService.updateStore(storeId, updateData);
-        return response;
-    } catch (error) {
-        console.error('Error updating store:', error);
-        throw error;
-    }
-}
+const storesUrl = "http://localhost:8001/stores";
+const ownerUrl = "http://localhost:8001/owners";
 
-async function deleteStore(storeId) {
-    try {
-        const response = await storeService.deleteStore(storeId);
-        return response;
-    } catch (error) {
-        console.error('Error deleting store:', error);
-        throw error;
-    }
-}
 
-async function getStoreById(storeId) {
+async function getStore() {
     try {
-        const response = await storeService.getStoreById(storeId);
-        return response;
+   
+    
     } catch (error) {
         console.error('Error fetching store by ID:', error);
         throw error;
     }
 }
-async function getAllOwnerStores(ownerId) {
-    try {
-        const response = await storeService.getAllOwnerStores(ownerId);
-        return response;
-    } catch (error) {
-        console.error('Error fetching all stores for owner:', error);
-        throw error;
-    }
-}
+
+
+
