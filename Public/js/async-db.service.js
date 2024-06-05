72dd9a67e8de9d5f1add833dff3998f088452601
NirForster <nirforster@gmail.com>
2024-06-05 16:10:01 +0300

    finished adding asynch functions to  asynchStoreOwner

--- Public/js/async-db.service.js
+++ Public/js/async-db.service.js
@@ -1,16 +1,64 @@
 const storesUrl = "http://localhost:8001/stores";
 const ownerUrl = "http://localhost:8001/owners";
 
-
-async function getStore() {
-    try {
-   
-    
-    } catch (error) {
-        console.error('Error fetching store by ID:', error);
-        throw error;
-    }
+export async function getStore() {
+  try {
+    const res = await axios.get(storesUrl);
+    console.log(res.data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
+export async function getStoreById(storeId) {
+  try {
+    const res = await axios.get(`${storesUrl}/${storeId}`);
+    console.log(res.data);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
+export async function postStore(storeData) {
+  try {
+    await axios.post(storesUrl, storeData);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
+export async function updateStore(storeId, updateStoreData) {
+  try {
+    const res = await axios.put(`${storesUrl}/${storeId}`, updateStoreData);
+    console.log(res.data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function deleteStore(storeId) {
+  try {
+    const res = await axios.deleteStore(`${storesUrl}/${storeId}`);
+    console.log(res.data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getAllOwnerStores(ownersId) {
+  try {
+    const res = await axios.get(`${ownerUrl}/${ownersId}`);
+    console.log(res.data.stores);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const storesFunc = {
+  getStore,
+  getStoreById,
+  postStore,
+  updateStore,
+  deleteStore,
+  getAllOwnerStores,
+};
---
+++ Public/js/controllers/stores.controller.js
@@ -0,0 +1,9 @@
+import { storesFunc } from "../async-db.service.js";
+
+// Example usage (you can remove these after testing)
+// storesFunc.getStore(); // Test get all stores function
+// // storesFunc.getStoreById("store-1"); // Test get store by ID function
+// }); // Test post store function
+// storesFunc.updateStore("store-1", { name: "Updated Store" }); // Test update store function
+// storesFunc.deleteStore("store-5"); // Test delete store function
+// storesFunc.getAllOwnerStores("owner-1"); // Test get all stores for a specific owner function
